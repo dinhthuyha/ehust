474da164390f97817dff9dc50142041b1c0b49f0
hai98 <haile197@gmail.com>
2022-05-27 23:35:40 +0700

    change localTime format

--- src/main/kotlin/com/hadt/ehust/response/ScheduleModel.kt
+++ src/main/kotlin/com/hadt/ehust/response/ScheduleModel.kt
@@ -1,10 +1,13 @@
 package com.hadt.ehust.response
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDate
 import java.time.LocalTime
 
 data class ScheduleModel(
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
     val startTime: LocalTime,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
     val finishTime: LocalTime,
     val dateStudy : String,
     val dueDateStudy: LocalDate
